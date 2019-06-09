a9356b8b6d442687e11f3943e7aaf9e0aed6dca3
Gabriel <gabrielgomesch@gmail.com>
2019-06-08 22:43:32 -0300

    script: menu controller script

---
+++ Assets/Scripts/Game Controllers/MenuController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public static MenuController instance;
+
+    [SerializeField]
+    private GameObject[] birds;
+
+    private bool isGreenBirdUnlocked, isRedBirdUnlocked;
+
+    void Awake()
+    {
+        MakeInstance();
+    }
+
+    void Start()
+    {
+        Debug.Log(birds);
+        Debug.Log(GameController.instance.GetSelectedBird());
+        birds[GameController.instance.GetSelectedBird()].SetActive(true);
+        CheckIfBirdsAreUnlocked();
+    }
+    
+    void MakeInstance()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlayGame(){
+        SceneFader.instance.FadeIn("GamePlay");
+    }
+
+    void CheckIfBirdsAreUnlocked()
+    {
+        if(GameController.instance.IsRedBirdUnlocked() != 1){
+            isRedBirdUnlocked = true;
+        }
+
+        if(GameController.instance.IsGreenBirdUnlocked() != 1){
+            isGreenBirdUnlocked = true;
+        }
+    }
+
+    public void ChangeBird()
+    {
+        Debug.Log("----");
+        Debug.Log(GameController.instance.IsRedBirdUnlocked());
+        Debug.Log(GameController.instance.IsGreenBirdUnlocked());
+
+        if(GameController.instance.GetSelectedBird() == 0)
+        {
+            if(isGreenBirdUnlocked){
+                birds[0].SetActive(false);
+                GameController.instance.SetSelectedBird(1);
+                birds[GameController.instance.GetSelectedBird()].SetActive(true);
+            }
+        } 
+        else if(GameController.instance.GetSelectedBird() == 1)
+        {
+            if(isRedBirdUnlocked){
+                birds[1].SetActive(false);
+                GameController.instance.SetSelectedBird(2);
+                birds[GameController.instance.GetSelectedBird()].SetActive(true);
+            } else 
+            {
+                birds[1].SetActive(false);
+                GameController.instance.SetSelectedBird(0);
+                birds[GameController.instance.GetSelectedBird()].SetActive(true);
+            }
+        }
+        else if(GameController.instance.GetSelectedBird() == 2)
+        {
+            birds[2].SetActive(false);
+            GameController.instance.SetSelectedBird(0);
+            birds[GameController.instance.GetSelectedBird()].SetActive(true);
+        }
+    }
+}
